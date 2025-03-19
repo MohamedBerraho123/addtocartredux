f71932aacbfc1eefec65becf62817433aaf31866
Mohameddev73 <takhlitasimo@gmail.com>
2025-03-19 04:15:55 +0100

    finish header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Badge from '@mui/material/Badge';
@@ -6,8 +6,12 @@ import Nav from 'react-bootstrap/Nav'
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+    const getdata = useSelector((state)=> state.cartreducer);
+    console.log(getdata);
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -41,13 +45,16 @@ const Header = () => {
           open={open}
           onClose={handleClose}
           MenuListProps={{
-            
             'aria-labelledby': 'basic-button',
           }}
         >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
+         <div className='card_details d-flex justify-content-center align-items-center' style={{width:"24rem",padding:10,position:"relative"}}>
+          <i className='fas fa-close smallclose'
+          onClick={handleClose}
+           style={{position:"absolute",top:2,right:20,fontSize:23,cursor:"pointer"}}></i>
+          <p style={{fontSize:22}}> Your carts is empty</p>
+          <img src='./cart.gif' alt='' className='emptycart_img' style={{width:"5rem",padding:10}} />
+         </div>
         </Menu>
       </Navbar>
     </>
